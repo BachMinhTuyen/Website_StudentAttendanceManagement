2683319d0381f6957905b7b777ba363945930300
Minh Tuyen <bachminhtuyen2003@gmail.com>
2024-06-09 00:27:24 +0700

    Subject & class management is completed

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,22 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->id('user_id');
+            $table->string('username')->unique();
             $table->string('password');
+            $table->string('type_of_user');
+            $table->string('faculty');
+            $table->string('avatar')->nullable();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('phone_number')->unique();
+            $table->string('address');
+            $table->dateTime('date_of_birth', precision: 0);
+
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
+            $table->timestamp('login_at')->nullable();
+            $table->timestamp('chage_password_at')->nullable();
             $table->timestamps();
         });
 
--- resources/views/student/student_home.blade.php
+++ resources/views/student/student_home.blade.php
@@ -32,15 +32,13 @@
             </div>
         </div>
         <hr class="border border-primary border-3 opacity-75">
-        <h3 class="text-primary">Danh sách lớp dạy học</h3>
-        <table class="table align-middle table-striped">
-            @if ($user->type_of_user == 'GV')
-            {{-- Xử lý cho giảng viên --}}
+        <h3 class="text-primary">Danh sách lớp học</h3>
+        <table class="table">
             <thead>
                 <tr class="table-secondary">
                     <th scope="col">Học phần</th>
                     <th scope="col">Tên lớp</th>
-                    <th scope="col">Số lượng sinh viên</th>
+                    <th scope="col">Tên giảng viên</th>
                     <th scope="col"></th>
                 </tr>
             </thead>
@@ -49,36 +47,14 @@
                 <tr>
                     <td>{{$schedule->subject_name}}</td>
                     <td>{{$schedule->class_name}}</td>
-                    <td>{{$schedule->number_of_members}}</td>
-                    <td>
-                        <a href="/schedule/{{$schedule->schedule_id}}" class="btn btn-primary">Quản lý</a>
-                    </td>
-                </tr>
-                @endforeach
-            @else 
-                {{-- Xử lý cho sinh viên --}}
-                <thead>
-                    <tr class="table-secondary">
-                        <th scope="col">Học phần</th>
-                        <th scope="col">Tên lớp</th>
-                        <th scope="col">Tên giảng viên</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                @foreach ($schedules as $schedule)
-                <tr>
-                    <td>{{$schedule->subject_name}}</td>
-                    <td>{{$schedule->class_name}}</td>
                     <td>{{$schedule->full_name}}</td>
                     <td>
-                        <a href="/schedule/{{$schedule->schedule_id}}" class="btn btn-primary">Tham gia</a>
+                        <a href="/schedule/{{$schedules->attendance_id}}" class="btn btn-primary">Tham gia</a>
                     </td>
                 </tr>
                 @endforeach
-            @endif
             </tbody>
         </table>
     </div>
 </div>
-@endsection 
+@endsection
